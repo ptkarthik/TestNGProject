f0f5cd0e209d4d8e94ce3df9e4f238fad17d528f
karthikeyan_thangave <pt.karthik1989@gmail.com>
2024-12-17 09:48:47 +0530

    commit jenkins file

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,17 +1,35 @@
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import guice.TestModule;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.time.Duration;
+
 public class BaseTest {
+
+    @Inject
+    WebDriver driver;
+
+    public static Injector getInjector() {
+        return injector;
+    }
+
     protected static Injector injector;
+
     public BaseTest() {
+        // Initialize the injector here
+
+
 
     }
+
     @BeforeMethod
     public void setUp() throws Exception {
-
+        injector = Guice.createInjector(new TestModule());
+        injector.injectMembers(this);
         /*this to be used for WebTables*
         driver.get("https://cosmocode.io/automation-practice-webtable/#google_vignette");
          */
