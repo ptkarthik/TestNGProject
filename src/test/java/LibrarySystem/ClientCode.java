1c07c820444aa8cbf91298505fadca937df93964
karthikeyan_thangave <pt.karthik1989@gmail.com>
2024-12-23 12:11:50 +0530

    commit jenkins file

---
+++ src/test/java/LibrarySystem/ClientCode.java
@@ -0,0 +1,12 @@
+package LibrarySystem;
+
+public class ClientCode {
+//    //
+//    Model a library system, with classes for books, member, and checkouts.
+//    The checkouts class should link a book and member together.
+//    Book - title, author
+//    Member - name, id, List checkoutbooks
+//    Checkout - book, member, checkoutDate, dueDate
+//
+//    Use streams to get max number of check out by each member
+}
