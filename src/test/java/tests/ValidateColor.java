8c5ceb8d4da30b2fdf89583bafa78f6737aca78d
karthikeyan_thangave <pt.karthik1989@gmail.com>
2024-12-23 11:09:58 +0530

    commit jenkins file

--- src/test/java/tests/ValidateColor.java
+++ src/test/java/tests/ValidateColor.java
@@ -5,15 +5,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.Color;
 import org.testng.annotations.Test;
 
-import java.util.regex.Matcher;
-
-public class ValidateCoolor extends BaseTest {
+public class ValidateColor extends BaseTest {
     @Test
     public void validateColor() {
         WebElement backGround = driver.findElement(By.xpath("//a[contains(text(),'Examples')]"));
-       String cloorString=backGround.getCssValue("color");
-        Color color= Color.fromString(cloorString);
-        String hexValue=color.asHex();
+        String colorString = backGround.getCssValue("color");
+        Color color = Color.fromString(colorString);
+        String hexValue = color.asHex();
         System.out.println(hexValue);
     }
 }
