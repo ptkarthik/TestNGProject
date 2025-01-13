9744e24406b39d5f60e704b2d8c945054f844ae7
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-13 10:23:38 +0530

    coomparator

--- src/test/java/practise/schoolsort/map/ClientCodeMap.java
+++ src/test/java/practise/schoolsort/map/ClientCodeMap.java
@@ -11,20 +11,27 @@ public class ClientCodeMap {
         student.put(4, new StudentDetails("Pradeep", "98900"));
         student.put(5, new StudentDetails("Harish", "878798"));
 
-        Set<Map.Entry<Integer, StudentDetails>> studentSet = student.entrySet();
-        for (Map.Entry<Integer, StudentDetails> iterator : studentSet) {
-            System.out.println(iterator.getValue().toString());
-        }
+//        Set<Map.Entry<Integer, StudentDetails>> studentSet = student.entrySet();
+//        for (Map.Entry<Integer, StudentDetails> iterator : studentSet) {
+//            System.out.println(iterator.getValue().toString());
+//        }
 
-//below returns collections
+        //below returns collections
         Collection<StudentDetails> collectionOfMap = student.values();
 
         List<StudentDetails> listOFStudents = new ArrayList<>(collectionOfMap);
-        System.out.println("The collections of list of students is" + listOFStudents);
-        for (StudentDetails j : listOFStudents) {
-            System.out.println(j.getStudentID());
-        }
-        Collections.sort(listOFStudents);
+//        System.out.println("The collections of list of students is" + listOFStudents);
+//
+//        //the below sorting is dne via student id as per the compare to method in StudentDetails Class
+//        //Collections.sort dooes that
+//        Collections.sort(listOFStudents);
+//        for (StudentDetails j : listOFStudents) {
+//            System.out.println(j.getStudentID());
+//        }
+
+        //lets see how reverse order works
+        Collections.sort(listOFStudents, new ComparatorReverseOrder());
+
         for (StudentDetails j : listOFStudents) {
             System.out.println(j.getStudentID());
         }
