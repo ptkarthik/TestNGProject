7ac32fc68e97b2cfc000338a03157302cde0dea5
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-13 10:12:15 +0530

    commit jenkins file

--- src/test/java/java8/DateTimeHandling/DateLearn.java
+++ src/test/java/java8/DateTimeHandling/DateLearn.java
@@ -2,8 +2,6 @@ package java8.DateTimeHandling;
 
 import org.testng.annotations.Test;
 
-import java.time.*;
-import java.time.temporal.Temporal;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -12,13 +10,8 @@ public class DateLearn {
     @Test
     public void loocalDate() {
         LocalDate localDate = LocalDate.now();
-        LocalDateTime localDateTime = LocalDateTime.now();
-//        LocalTime
-//        Tempoal
-ZoneId.of("Asia");
 
         System.out.println(localDate.plusDays(3));
-        System.out.println(ZonedDateTime.of(localDateTime, ZoneId.of("Asia/Kolkata")));
         ZonedDateTime.of(localDate.atTime(22, 30), ZoneId.of("Asia/Kolkata"));
     }
 
