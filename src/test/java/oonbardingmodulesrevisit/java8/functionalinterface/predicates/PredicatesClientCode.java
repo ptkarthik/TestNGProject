f26911a771b6388c2c868c9e81c5957137c48344
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-17 12:27:39 +0530

    coomparator

--- src/test/java/oonbardingmodulesrevisit/java8/functionalinterface/predicates/PredicatesClientCode.java
+++ src/test/java/oonbardingmodulesrevisit/java8/functionalinterface/predicates/PredicatesClientCode.java
@@ -4,6 +4,7 @@ import oonbardingmodulesrevisit.java8.ProductClass;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class PredicatesClientCode {
     /*
@@ -13,7 +14,34 @@ public class PredicatesClientCode {
      */
     public static void main(String[] args) {
 
-        List<ProductClass> listOfProducts= new ArrayList<>();
-        listOfProducts.add(new ProductClass("Detergent",34.55,"HomeProducts",'A'));
+        List<ProductClass> listOfProducts = new ArrayList<>();
+        listOfProducts.add(new ProductClass("Detergent", 34.55, "HomeProducts", 'H'));
+        listOfProducts.add(new ProductClass("Sony TV", 89999, "Electronics", 'E'));
+        listOfProducts.add(new ProductClass("Washing Machine", 14999, "Home Appliances", 'H'));
+        listOfProducts.add(new ProductClass("HeadSet-JBL", 788.50, "Electronics", 'E'));
+        listOfProducts.add(new ProductClass("Cakes-plum", 249.99, "Foods", 'F'));
+        listOfProducts.add(new ProductClass("25 KG Rice", 788.50, "Foods", 'F'));
+        listOfProducts.add(new ProductClass("Laptop Keyboard", 499.99, "Electronics", 'E'));
+
+        listOfProducts.add(new ProductClass("Toothbrush", 18.99, "HomeProducts", 'H'));
+        listOfProducts.add(new ProductClass("Biscuits and Snakes", 145.99, "Foods", 'F'));
+
+        //approach 1 without streams
+        Predicate<ProductClass> product = p -> p.getPrice() > 100 &&
+                p.getCategory().equalsIgnoreCase("Electronics");
+
+        for (ProductClass products : listOfProducts) {
+            if (product.test(products)) {
+                System.out.println(products.toString());
+            }
+
+            //approach 2: with streams
+            System.out.println("Below is via Streams");
+            listOfProducts.stream().filter(p -> p.getPrice() > 100 &&
+                            p.getCategory().equalsIgnoreCase("Electronics")).
+                    forEach(p -> System.out.println(p.toString()));
+
+        }
+
     }
 }
