ed1c6da72e878066df5c50169d59dbc1e38eb7d5
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-17 23:47:20 +0530

    coomparator

--- src/test/java/practise/restassured/hometask2/VerifyTypeCode.java
+++ src/test/java/practise/restassured/hometask2/VerifyTypeCode.java
@@ -1,11 +1,18 @@
 package practise.restassured.hometask2;
 
+
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class VerifyTypeCode {
     /*
     Task
-1. Verify the number of resources at each resource end point (6 marks)
+
 2. For each type of resource
 	a. Get a specific resource data (6 marks)
 		i. verify the response code
@@ -23,6 +30,14 @@ public class VerifyTypeCode {
      */
     @Test
     public void typeCode() {
+        RequestSpecification baseUri = given().baseUri("https://jsonplaceholder.typicode.com");
+//1. Verify the number of resources at each resource end point (6 marks)
+        //resource at post
+        Response response = given().spec(baseUri).when().get("/posts").then().extract().response();
+        //approach1:
+        int size = response.jsonPath().getList("$").size();
+        System.out.println(size);
+
 
     }
 }
