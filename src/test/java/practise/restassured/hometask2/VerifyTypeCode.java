de6d559aae488b6dcbd3073398e526599586c45e
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-17 23:29:01 +0530

    coomparator

---
+++ src/test/java/practise/restassured/hometask2/VerifyTypeCode.java
@@ -0,0 +1,28 @@
+package practise.restassured.hometask2;
+
+import org.testng.annotations.Test;
+
+public class VerifyTypeCode {
+    /*
+    Task
+1. Verify the number of resources at each resource end point (6 marks)
+2. For each type of resource
+	a. Get a specific resource data (6 marks)
+		i. verify the response code
+		ii. Verify the response body
+	b. Modify a specific resource data (6 marks)
+		i. verify the response code
+		ii. Verify the response body
+	c. Delete a specific resource (6 marks)
+		i. verify the response code
+		ii. Verify the response body
+	d. Create your own resource (6 marks)
+		i. verify the response code
+		ii. Verify the response body
+
+     */
+    @Test
+    public void typeCode() {
+
+    }
+}
