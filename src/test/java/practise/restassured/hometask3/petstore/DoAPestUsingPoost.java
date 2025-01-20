5521b2c208487932160cbc74ffa1b901b0ed6514
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-20 10:17:36 +0530

    assertion to be done yet

--- src/test/java/practise/restassured/hometask3/petstore/DoAPestUsingPoost.java
+++ src/test/java/practise/restassured/hometask3/petstore/DoAPestUsingPoost.java
@@ -18,8 +18,8 @@ public class DoAPestUsingPoost {
     public void createAPet() {
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
         RequestSpecification initialData = requestSpecBuilder.setBaseUri("https://petstore.swagger.io").
-                addPathParam("version", "v2").
                 addPathParam("petType", "pet").
+                addPathParam("version", "v2").
                 addHeader("Content-Type", "application/json").build();
         //create an object for category
         Category category = new Category(0, "string");
@@ -31,9 +31,21 @@ public class DoAPestUsingPoost {
 
         //create an Object for PojoPet
         PojoForPet pojoForPet = new PojoForPet(0, category,
-                "doggie", Arrays.asList("string"), listOfCategory, "available");
+                "Karthik's Dog", Arrays.asList("string"), listOfCategory, "available");
 
+        //getting post response
         Response response = given().spec(initialData).body(pojoForPet).log().all().when().
                 post("/{version}/{petType}").then().log().all().extract().response();
+
+        //using the response we receive we get the id and pass that back as get request
+        RequestSpecification latterData = requestSpecBuilder.setBaseUri("https://petstore.swagger.io").
+                addPathParam("petType", "pet").
+                addPathParam("version", "v2").
+                addPathParam("petID", response.jsonPath().get("id")).build();
+        given().spec(latterData).when().get("/{version}/{petType}/{petID}").then().log().all();
+
+
     }
+
+
 }
