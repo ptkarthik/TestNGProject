e96cc7ed87b4bbae8cb977d04302d14abb4b395d
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-21 15:28:35 +0530

    java8

--- src/test/java/practise/collectionsproogrampractise/java/CustomCollectorToCountVowelsInAList.java
+++ src/test/java/practise/collectionsproogrampractise/java/CustomCollectorToCountVowelsInAList.java
@@ -1,4 +1,9 @@
 package practise.collectionsproogrampractise.java;
 
 public class CustomCollectorToCountVowelsInAList {
+
+    //count of vowels example lets see. list os strings that has some strings.
+    //we have too provide a map that wil jey and string and value as the count oof vowels it has goot
+
+
 }
--- src/test/java/practise/collectionsproogrampractise/java8/CoonvertListToMap.java
+++ src/test/java/practise/collectionsproogrampractise/java8/CoonvertListToMap.java
@@ -9,15 +9,24 @@ import java.util.stream.Collectors;
 
 public class CoonvertListToMap {
     public static void main(String[] args) {
-
-
         List<Integer> listOfIntegers = Arrays.asList(1, 43, 4, 5, 112, 11, 3, 9, 34, 22, 45, 6);
         List<String> listOfString = Arrays.asList("Karthik", "Ajay", "Harish", "Jeevan",
                 "Kanmani", "Ajith", "Vijay");
         SecureRandom secureRandom = new SecureRandom();
         Map<Integer, String> map =
                 listOfIntegers.stream().collect(Collectors.
-                        toMap(e -> e, e -> listOfString.get(secureRandom.nextInt(listOfString.size()))));
+                        toMap(e -> e, e -> listOfString.get(secureRandom.nextInt(listOfString.size())), (d, g) ->
+                                d, LinkedHashMap::new));
         System.out.println(map);
+
+
+        //using reduce and find max value of
+        //logic is 0 is identity and a is the same identity, b is the value stream has. one by one.
+        /*
+        first iteration it checks 0,1
+        second iteration since 1 is greater it becomes a and b is the next value of stream it keeps checking and return
+         */
+        Integer max = listOfIntegers.stream().reduce(0, (a, b) -> a > b ? a : b);
+        System.out.println("the max" + max);
     }
 }
