185eaa205ed9e63682120b395ab77d1c22938d1b
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-21 13:38:44 +0530

    map

--- src/test/java/practise/collectionsproogrampractise/java/CoonvertListToMap.java
+++ src/test/java/practise/collectionsproogrampractise/java/CoonvertListToMap.java
@@ -1,4 +1,22 @@
 package practise.collectionsproogrampractise.java;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CoonvertListToMap {
+    public static void main(String[] args) {
+        List<Integer> listOfIntegers = Arrays.asList(1, 43, 4, 5, 112, 11, 3, 4, 9, 34, 22, 45, 6);
+        List<String> listOfString = Arrays.asList("Karthik", "Ajay", "Harish", "Jeevan",
+                "Kanmani", "Ajith", "Vijay");
+        Map<Integer, String> map = new HashMap<>();
+        SecureRandom secureRandom = new SecureRandom();
+        for (Integer s : listOfIntegers) {
+            map.put(s, listOfString.get(secureRandom.nextInt(listOfString.size())));
+        }
+
+        System.out.println(map);
+    }
 }
--- src/test/java/practise/collectionsproogrampractise/java8/CoonvertListToMap.java
+++ src/test/java/practise/collectionsproogrampractise/java8/CoonvertListToMap.java
@@ -1,4 +1,23 @@
 package practise.collectionsproogrampractise.java8;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class CoonvertListToMap {
+    public static void main(String[] args) {
+
+
+        List<Integer> listOfIntegers = Arrays.asList(1, 43, 4, 5, 112, 11, 3, 9, 34, 22, 45, 6);
+        List<String> listOfString = Arrays.asList("Karthik", "Ajay", "Harish", "Jeevan",
+                "Kanmani", "Ajith", "Vijay");
+        SecureRandom secureRandom = new SecureRandom();
+        Map<Integer, String> map =
+                listOfIntegers.stream().collect(Collectors.
+                        toMap(e -> e, e -> listOfString.get(secureRandom.nextInt(listOfString.size()))));
+        System.out.println(map);
+    }
 }
