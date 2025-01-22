65910579c5a20d41d9d5d2a45389f1733d0e562e
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-22 18:20:24 +0530

    stream pojoo validatin

---
+++ src/test/java/practise/selectorshubwebpageautomation/AutomatingThePage.java
@@ -0,0 +1,11 @@
+package practise.selectorshubwebpageautomation;
+
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class AutomatingThePage extends BaseTest {
+    @Test
+    public void test() {
+
+    }
+}
