42bc6f7258c98a1801470bfacdcf1e75863a4ae6
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-24 19:17:19 +0530

    added list validation

---
+++ src/test/java/practise/practiseroundtwo/java8/FilterAndCollectData.java
@@ -0,0 +1,7 @@
+package practise.practiseroundtwo.java8;
+
+public class FilterAndCollectData extends RuntimeException {
+    public FilterAndCollectData(String message) {
+        super(message);
+    }
+}
