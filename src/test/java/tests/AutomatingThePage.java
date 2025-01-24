ed57cde16c4d2fed7dc4d9f6d3ea5a434ad126e6
karthikeyan_thangave <pt.karthik1989@gmail.com>
2025-01-24 10:36:27 +0530

    added list validation

--- src/test/java/tests/AutomatingThePage.java
+++ src/test/java/tests/AutomatingThePage.java
@@ -1,9 +1,11 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AutomatingThePage extends BaseTest {
+
     @Test(priority = 1, groups = {"Smoke", "Regression"}, enabled = true,dependsOnMethods ="tests.CalendarValidations.calendarTest" )
     public void test() {
         driver.findElement(By.xpath("//input[@name='password'])"))
