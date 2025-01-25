e5bfe5d75817ccc90283d218df32c6b99fc2a12a
Karthik <pt.karthik1989@gmail.com>
2025-01-25 14:02:50 +0530

    generics

--- src/test/java/practise/collectionsproogrampractise/java8/GroupStringByLengthUsingMap.java
+++ src/test/java/practise/collectionsproogrampractise/java8/GroupStringByLengthUsingMap.java
@@ -1,4 +1,28 @@
 package practise.collectionsproogrampractise.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GroupStringByLengthUsingMap {
+    public static void main(String args[]) {
+
+        List<String> strings = Arrays.asList(
+                "Code",     // Length 4
+                "Apple",    // Length 5
+                "Sun",      // Length 3
+                "Hi",       // Length 2
+                "Test",     // Length 4
+                "Star",     // Length 4
+                "Moon",     // Length 4
+                "Planet",   // Length 6
+                "Java",     // Length 4
+                "Rocket",   // Length 6
+                "Sky",      // Length 3
+                "A"         // Length 1
+        );
+
+//        strings.stream().collect()
+
+    }
 }
